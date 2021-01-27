4ffa834273c59923d5e2ad844146f6f98426b405
GugaMonteiro <luizfmonteiro@outlook.com>
2021-01-27 16:31:12 -0300

    Exercise Complete.

--- 01_SequentialEstructure/Exercise_17.py
+++ 01_SequentialEstructure/Exercise_17.py
@@ -11,16 +11,33 @@ gallon = 3.6         # 1 gallon contain 3.6 liters
 
 gallon_meters = gallon * float(limit)
 
+paint_mix = (can_meters + gallon_meters) * 1.1
+
 can_price = 80
 gallon_price = 25
 
 if paint_type == 1:
-    total = area % can_meters
-    if total == 1:
-        print(f"\nYou have to buy {total} can for $ 80,00.")
+    total = area / can_meters
+    if 0 < total < 1:
+        print(f"\nYou have to buy {round(total+0.5)} can for ${float(can_price)}.")
     else:
-        q_can = area / can_meters       # can quantity
-        final_price = float(total) * can_price
-        round(final_price+0.5)
-        print(f"You have to buy {q_can} cans for ${final_price}")
-
+        total = area / can_meters
+        final_price = round(total+0.5) * can_price
+        print(f"You have to buy {round(total+0.5)} cans for ${round(final_price, 2)}")
+elif paint_type == 2:
+    total = area / gallon_meters
+    if 1 < total < 0:
+        print(f"\nYou have to buy {round(total+0.5)} gallon for ${float(gallon_price)}.")
+    else:
+        total = area / gallon_meters
+        final_price = round(total+0.5) * gallon_price
+        print(f"\nYou have to buy {round(total+0.5)} gallon for ${round(final_price, 2)}.")
+elif paint_type == 3:
+    total = area / paint_mix
+    if 1 < total < 0:
+        print(f"\nYou have to buy {round(total+0.5)} mixed types of cans.")
+    else:
+        total = area / paint_mix
+        print(f"You have to buy {round(total+0.5)} mixed types of cans.")
+else:
+    print("Something gone wrong. Try again.")
